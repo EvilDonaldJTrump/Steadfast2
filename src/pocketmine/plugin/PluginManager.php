ff14290bd2bcff26de6e1ff3761de696d46d9a06
Keith <keith@imagicalmine.net>
2017-03-30 22:41:29 +1100

    fix

--- src/pocketmine/plugin/PluginManager.php
+++ src/pocketmine/plugin/PluginManager.php
@@ -218,18 +218,18 @@ class PluginManager{
 								//Format: majorVersion.minorVersion.patch
 								$version = array_map("intval", explode(".", $version));
 								$apiVersion = array_map("intval", explode(".", $this->server->getApiVersion()));
-								//Completely different API version
-								if($version[0] !== $apiVersion[0]){
-									continue;
-								}
-								//If the plugin requires new API features, being backwards compatible
-								if($version[1] > $apiVersion[1]){
-									continue;
-								}
- 								//If the plugin uses new API
-								if($version[0] < $apiVersion[0]){
+ 								//Completely different API version
+  								if($version[0] > $apiVersion[0]){
+  									continue;
+  								}
+  								//If the plugin uses new API
+  								if($version[0] < $apiVersion[0]){
  									$compatible = true;
   									break;
+ 								}
+  								//If the plugin requires new API features, being backwards compatible
+ 								if($version[1] > $apiVersion[1]){
+ 									continue;
   								}
 
 								$compatible = true;
